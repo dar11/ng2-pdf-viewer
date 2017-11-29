b1b4f80e6d4d04e199500fb285f41cc0cb5a498e
Daniel Arnsberger <daniel.arnsberger@googlemail.com>
2017-11-29 11:08:04 +0100

    Use local pdf worker file

--- build/pdf-viewer/pdf-viewer.component.js
+++ build/pdf-viewer/pdf-viewer.component.js
@@ -28,7 +28,7 @@ var PdfViewerComponent = (function () {
         this.onProgress = new core_1.EventEmitter();
         this.pageChange = new core_1.EventEmitter(true);
         if (!isSSR()) {
-            PDFJS.workerSrc = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/" + PDFJS.version + "/pdf.worker.min.js";
+            PDFJS.workerSrc = 'lib/pdfjs-dist/build/pdf.worker.min.js';
         }
     }
     PdfViewerComponent.prototype.ngOnInit = function () {
--- src/pdf-viewer/pdf-viewer.component.ts
+++ src/pdf-viewer/pdf-viewer.component.ts
@@ -297,7 +297,8 @@ export class PdfViewerComponent implements OnChanges, OnInit {
 
   constructor(private element: ElementRef) {
     if (!isSSR()) {
-      PDFJS.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${ (PDFJS as any).version }/pdf.worker.min.js`;
+      //PDFJS.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${ (PDFJS as any).version }/pdf.worker.min.js`;
+      PDFJS.workerSrc = './../lib/pdfjs-dist/build/pdf.worker.min.js';
     }
   }
 
